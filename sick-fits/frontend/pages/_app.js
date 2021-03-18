79102dc6674acc7967a088e54305ffe6ff1dbaa2
Tim H <timh1203@gmail.com>
2021-03-17 21:03:55 -0500

    Add NProgress loading visualization

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,14 @@
-import Page from "../components/Page";
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Page from '../components/Page';
+import '../components/styles/nprogress.css'
+
+// Default Nprogress. We brought in our own.
+// import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
     return <Page>
