de1c342c2fc7c0a050e7d8df59bd33977c2a6e62
Tim H <timh1203@gmail.com>
2021-03-20 11:08:46 -0500

    Add apollo provider and fix _document error

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -2,6 +2,8 @@ import NProgress from 'nprogress';
 import Router from 'next/router';
 import Page from '../components/Page';
 import '../components/styles/nprogress.css'
+import { ApolloProvider } from '@apollo/client';
+import withData from '../lib/withData';
 
 // Default Nprogress. We brought in our own.
 // import 'nprogress/nprogress.css';
@@ -10,8 +12,22 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
-    return <Page>
-        <Component {...pageProps} />
-    </Page>
+function MyApp({ Component, pageProps, apollo }) {
+    return (
+        <ApolloProvider client={apollo}>
+            <Page>
+                <Component {...pageProps} />
+            </Page>
+        </ApolloProvider>
+    )
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+export default withData(MyApp);
