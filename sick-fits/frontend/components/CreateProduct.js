eb379646d937046ea8901f00bb8ba9131e72f12c
Tim H <timh1203@gmail.com>
2021-03-23 22:03:14 -0500

    Adding mutations to save products

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,5 +1,40 @@
+import { useMutation } from "@apollo/client";
+import gql from "graphql-tag";
 import useForm from "../lib/useForm";
+import DisplayError from "./ErrorMessage";
 import Form from "./styles/Form";
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+      # Which variables are passed in and their types?
+      # Bang operators means required.
+      # Think of all this as graphql, not JS.
+      $name: String!
+      $description: String!
+      $price: Int!
+      $image: Upload
+  ) {
+	createProduct(
+    data: {
+      name: $name
+      description: $description
+      price: $price
+      status: "AVAILABLE"
+      photo: {
+        create: {
+          image: $image,
+          altText: $name
+        }
+      }
+    }
+    ) {
+        id
+        price
+        description
+        name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -9,14 +44,23 @@ export default function CreateProduct() {
     description: "Item description",
   });
 
+  const [createProduct, { loading, error, data}] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  )
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
         console.log(inputs);
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input required type="file" id="image" name="image" onChange={handleChange} />
