d8170ea6afdf5f10b77ea3ed1324377bd5e314a6
Tim H <timh1203@gmail.com>
2021-03-24 22:32:13 -0500

    Add routing, render, stylings for singleProduct

---
+++ sick-fits/frontend/components/SingleProduct.js
@@ -0,0 +1,65 @@
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import DisplayError from "./ErrorMessage";
+import Head from 'next/head'
+import styled from "styled-components";
+
+const ProductStyles = styled.div`
+    display: grid;
+    grid-auto-columns: 1fr;
+    grid-auto-flow: column;
+    max-width: var(--maxWidth);
+    align-items: center;
+    gap: 2rem;
+    img {
+        width: 100%;
+        object-fit: contain;
+    }
+`;
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!){
+    Product(where: { id: $id }) {
+      name
+      price
+      description
+      id
+      photo {
+          altText
+          image {
+              publicUrlTransformed
+          }
+      }
+    }
+  }
+`;
+
+
+export default function SingleProduct({ id }) {
+    const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY, {
+        variables: {
+            id
+        }
+    });
+    
+    if (loading) return <p>loading...</p>
+    if (error) return <DisplayError />
+
+    const { Product } = data;
+
+    return (
+        <ProductStyles>
+            <Head>
+                <title></title>
+            </Head>
+            <img
+                src={Product.photo.image.publicUrlTransformed}
+                alt={Product.photo.altText}
+            />
+            <div className="details">
+                <h2>{Product.name}</h2>
+                <p>{Product.description}</p>
+            </div>
+        </ProductStyles>
+    )
+}
